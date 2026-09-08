11bf3d49ae9cef42b57cb7be084679b37bc1da28
Dinesh Perukuri <dineshperukuri26@gmail.com>
2026-09-08 11:01:24 +0530

    Introduction of Strings

---
+++ Day-06/Strings.py
@@ -0,0 +1,249 @@
+Python 3.14.6 (tags/v3.14.6:c63aec6, Jun 10 2026, 10:26:10) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+#String operations
+#
+#Concatination
+s='codegnan'
+a
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+s
+'codegnan'
+type(s)
+<class 'str'>
+s=''
+s
+''
+a='python'
+b='progress'
+a+b
+'pythonprogress'
+fname=Dinesh
+Traceback (most recent call last):
+  File "<pyshell#12>", line 1, in <module>
+    fname=Dinesh
+NameError: name 'Dinesh' is not defined
+fname='Dinesh'
+lname='Perukuri'
+fname+lname
+'DineshPerukuri'
+#Reputation
+a*100
+'pythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpythonpython'
+'*'10
+SyntaxError: invalid syntax
+'*'*10
+'**********'
+'-Dinesh-'*999'
+SyntaxError: unterminated string literal (detected at line 1)
+'-Dinesh-'*999
+'-Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh--Dinesh-'
+#Indexing +ve indexing - indexing
++
+SyntaxError: invalid syntax
+#Indexing +ve indexing -ve indexing
+s='Dinesh'
+s[6]
+Traceback (most recent call last):
+  File "<pyshell#26>", line 1, in <module>
+    s[6]
+IndexError: string index out of range
+s[8]
+Traceback (most recent call last):
+  File "<pyshell#27>", line 1, in <module>
+    s[8]
+IndexError: string index out of range
+s[5]
+'h'
+s[-1:-5]
+''
+names= Dinesh, Abhi, Vikky
+Traceback (most recent call last):
+  File "<pyshell#30>", line 1, in <module>
+    names= Dinesh, Abhi, Vikky
+NameError: name 'Dinesh' is not defined
+names='Dinesh', 'Abhi', 'Vikky'
+names
+('Dinesh', 'Abhi', 'Vikky')
+names=[7]
+names
+[7]
+names[0;7]
+SyntaxError: invalid syntax
+names[0:7]
+[7]
+names='Dinesh', 'Abhi', 'Vikky'
+names[0:7]
+('Dinesh', 'Abhi', 'Vikky')
+names[1:4]
+('Abhi', 'Vikky')
+Dinesh[-1:7]
+Traceback (most recent call last):
+  File "<pyshell#40>", line 1, in <module>
+    Dinesh[-1:7]
+NameError: name 'Dinesh' is not defined
+names[-1:-7]
+()
+names[-1:-4]
+()
+names[-2:-5]
+()
+names[-5]
+Traceback (most recent call last):
+  File "<pyshell#44>", line 1, in <module>
+    names[-5]
+IndexError: tuple index out of range
+names[-5:]
+('Dinesh', 'Abhi', 'Vikky')
+#Slicing
+#Membership
+in not in
+SyntaxError: invalid syntax
+
+names[::-1]
+('Vikky', 'Abhi', 'Dinesh')
+#sorted max min
+len(names)
+3
+ord('d')
+100
+ord('h')
+104
+ord('m')
+109
+ord('a')
+97
+chr(10)
+'\n'
+sorted(names)
+['Abhi', 'Dinesh', 'Vikky']
+max(names)
+'Vikky'
+min(names)
+'Abhi'
+max(d)
+Traceback (most recent call last):
+  File "<pyshell#61>", line 1, in <module>
+    max(d)
+NameError: name 'd' is not defined. Did you mean: 'id'?
+#Case conversion method
+#Uppercase lowercase firstcase captalize title
+#Casefold
+s='Python programing language'
+s.upper()
+'PYTHON PROGRAMING LANGUAGE'
+s.lower()
+'python programing language'
+s.swapcase()
+'pYTHON PROGRAMING LANGUAGE'
+s.captialize()
+Traceback (most recent call last):
+  File "<pyshell#69>", line 1, in <module>
+    s.captialize()
+AttributeError: 'str' object has no attribute 'captialize'. Did you mean: 'capitalize'?
+s.captialize()
+Traceback (most recent call last):
+  File "<pyshell#70>", line 1, in <module>
+    s.captialize()
+AttributeError: 'str' object has no attribute 'captialize'. Did you mean: 'capitalize'?
+s.capitialize()
+Traceback (most recent call last):
+  File "<pyshell#71>", line 1, in <module>
+    s.capitialize()
+AttributeError: 'str' object has no attribute 'capitialize'. Did you mean: 'capitalize'?
+s.capitalize()
+'Python programing language'
+s.title()
+'Python Programing Language'
+#Alingment methods center, left-ljust, right-rlist, z-fill
+s
+'Python programing language'
+s.center(90'#')
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+s.center(90,'#')
+'################################Python programing language################################'
+s.ljust(50,'_')
+'Python programing language________________________'
+s.rjust(900,'^')
+'^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^Python programing language'
+'963'.zfill(8)
+'00000963'
+'158496'(70)
+Traceback (most recent call last):
+  File "<pyshell#81>", line 1, in <module>
+    '158496'(70)
+TypeError: 'str' object is not callable
+'159873'.zfill(70)
+'0000000000000000000000000000000000000000000000000000000000000000159873'
+#Search and Find methods find, index, count,
+s
+'Python programing language'
+s.find('python')
+-1
+s.finf('g')
+Traceback (most recent call last):
+  File "<pyshell#86>", line 1, in <module>
+    s.finf('g')
+AttributeError: 'str' object has no attribute 'finf'. Did you mean: 'find'?
+s.find('g')
+10
+s.find('p')
+7
+s.rfind('g')
+24
+s,rfind('a')
+Traceback (most recent call last):
+  File "<pyshell#90>", line 1, in <module>
+    s,rfind('a')
+NameError: name 'rfind' is not defined. Did you mean: 'round'?
+>>> s.find('z')
+-1
+>>> s.find('a')
+12
+>>> s.index('a')
+12
+>>> a.rindex('a')
+Traceback (most recent call last):
+  File "<pyshell#94>", line 1, in <module>
+    a.rindex('a')
+ValueError: substring not found
+>>> a.lindex('a')
+Traceback (most recent call last):
+  File "<pyshell#95>", line 1, in <module>
+    a.lindex('a')
+AttributeError: 'str' object has no attribute 'lindex'. Did you mean: 'index'?
+>>> s.rindex('a')
+23
+>>> s.lindex('a')
+Traceback (most recent call last):
+  File "<pyshell#97>", line 1, in <module>
+    s.lindex('a')
+AttributeError: 'str' object has no attribute 'lindex'. Did you mean: 'index'?
+>>> s.count('m')
+1
+>>> s.count('s')
+0
+>>> #Replace and
+>>> 
+>>> 
+>>> s.replace('0','1')
+'Python programing language'
+>>> s.replace('m','2')
+'Python progra2ing language'
+>>> s.replace('python','java')
+'Python programing language'
+>>> s.marktrans('aeiou','#@$%^')
+Traceback (most recent call last):
+  File "<pyshell#107>", line 1, in <module>
+    s.marktrans('aeiou','#@$%^')
+AttributeError: 'str' object has no attribute 'marktrans'. Did you mean: 'maketrans'?
+>>> s.maketrans('aeiou','#@$%^')
+{97: 35, 101: 64, 105: 36, 111: 37, 117: 94}
+>>> #Translate
+>>> s.translate(s.maketrans('aeiou','#@$%^'))
+'Pyth%n pr%gr#m$ng l#ng^#g@'
+>>> text='Hello '
+>>> text.translate
+<built-in method translate of str object at 0x000001E750D8C420>
