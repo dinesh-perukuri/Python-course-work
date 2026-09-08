9c14743e7cef17ed12f561bcb96b7f118ecd7d6c
Dinesh Perukuri <dineshperukuri26@gmail.com>
2026-09-08 11:13:25 +0530

    Conditional Statements

---
+++ Day-10/if.py
@@ -0,0 +1,20 @@
+'''
+sales = int(input("Enter the sales: "))
+
+if sales > 1000:
+    print("Best Seller")
+    '''
+
+'''
+eli_acc = eval(input("Eligible Account: "))
+ver_sub = eval(input("Meta Verified Subscription: "))
+
+if eli_acc and ver_sub :
+    print(" Verified Badge Granted")
+'''
+
+rain_status = input("Is it raining? (yes/no): ")
+if rain_status == "yes": 
+    print("Carry an umbrella")
+else:
+    print("No need for an umbrella")        
